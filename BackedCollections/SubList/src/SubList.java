a55fb05b624c508a747e02ea1552b0f94a7705ea
Adriano Jesus <adriano.jesus2@fatec.sp.gov.br>
2018-07-31 11:11:39 -0300

    Detailing a working example of a Backed Sub List.

--- BackedCollections/SubList/src/SubList.java
+++ BackedCollections/SubList/src/SubList.java
@@ -1,34 +1,59 @@
 import java.util.*;
 import java.util.function.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class SubList {
+
+	public static void print(List<? extends Object> list) {
+		
+		System.out.println(list.getClass());
+		System.out.print("Content: ");
+		for (int i = 0 ; i < list.size(); i++) {
+			System.out.print(list.get(i));
+			if (i < list.size() -1)
+				System.out.print(", ");
+			else
+				System.out.println("\n");
+		}
+	}
+
 	public static void main(String...args) {
+
 		List<String> vowels = new ArrayList<String>();
 		vowels.add("a"); 
 		vowels.add("e"); 
 		vowels.add("i"); 
 		vowels.add("o"); 
 		vowels.add("u"); 
-		Function<List<String>, List<String>> f = list->list.subList(2, 4); 
-		List<String> subList = f.apply(vowels); 
+
+		System.out.println("Main list (vowels):");
+		print(vowels);
+
+// 		This would resolve the ConcurrentModificationException, but it would not be a backed list.
+//		List<String> subList = new CopyOnWriteArrayList<String>(vowels.subList(2, 4)); 
+
+		List<String> subList = vowels.subList(2, 4);
+		System.out.println("List subList = vowels.subList(2, 4)");
+
 		subList.add("B");
-
-		subList.forEach(System.out::print);
-		System.out.println();
-		vowels.forEach(System.out::print);
-		System.out.println("\n");
-
-		vowels.add("L");
-
-		// This wont compile bacause it isnt a CopyOnWriteArrayList
-		// and forEach uses Iterator
-		//subList.forEach(System.out::print);
-		//for (int i = 0; i < subList.size(); i++)
-		//System.out.print(subList.get(i));
+		System.out.println("subList.add(\"B\")");
 		
-		System.out.println();
-		vowels.forEach(System.out::print);
+		System.out.println("SubList: ");
+		print(subList);
 
+		System.out.println("Main list (vowels):");	
+		print(vowels);
+		
+		vowels.add("L");
+		System.out.println("vowels.add(\"L\")");
 
+		System.out.println("Main list (vowels):");
+		print(vowels);
+
+		// Starting to this point, is not possible to access the subList, 
+		// because its originator, vowels, was altered (vowels.add("L")), so, 
+		// if we attempt to access the subList, it will throw a ConcurrentModificationException
+//		System.out.println("Sublist: ");
+//		print(subList);
 	}
 }
